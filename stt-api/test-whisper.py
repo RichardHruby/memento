a8b1554ccf54a567465d0e0ac84075f464d67ccb
AarushSah <aarush.sah@gmail.com>
2024-04-06 12:49:14 -0700

    Example of speaker recognition - Whisper segments based on voice.

--- stt-api/test-whisper.py
+++ stt-api/test-whisper.py
@@ -1,12 +1,21 @@
 from openai import OpenAI
+import pprint
+import time
 client = OpenAI()
+start_reading = time.time()
+audio_file = open("stt-api/memento-sample-audio.mp3", "rb")
+end_reading = time.time()
+print(f"Reading audio file took {end_reading - start_reading} seconds")
 
-audio_file = open("speech.mp3", "rb")
+start_transcription = time.time()
 transcript = client.audio.transcriptions.create(
   file=audio_file,
   model="whisper-1",
   response_format="verbose_json",
-  timestamp_granularities=["word"]
+  timestamp_granularities=["segment"]
 )
+end_transcription = time.time()
+print(f"Transcription took {end_transcription - start_transcription} seconds")
 
-print(transcript.words)
+print(transcript)
+pprint.pp(str(transcript))
